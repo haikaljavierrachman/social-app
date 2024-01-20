6ba9be09b4cc350db9bcf2350473adb0e8479e55
Haikal Javier Rachman <haikaljavierrachman@gmail.com>
2024-01-20 17:00:22 +0700

    testing

--- auth/src/__test__/index.test.ts
+++ auth/src/__test__/index.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { app } from "../app";
+import app from "../app";
 
 it('Respond with a status code 200', () => {
     request(app)
--- auth/src/app.ts
+++ auth/src/app.ts
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
     res.status(200).send();
 });
 
-export { app };
+export default app;
