ed52cc16619f7ba6dcc35b8855f6dd4cb87dbf53
Haikal Javier Rachman <haikaljavierrachman@gmail.com>
2024-01-31 12:34:08 +0700

    feat(auth): Password Validation

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -5,11 +5,101 @@ import app from '../../app';
 
 // });
 
-it('should return 422 if the email is not valid', async () => {
-  await request(app).post('/api/auth/signup').send({}).expect(422);
+/**
+ * Valid email condition:
+ *  -- Standar email format from 'express-validator'
+*/ 
+describe('test validity of email input', () => {
+  let password = '';
 
-  await request(app)
-    .post('/api/auth/signup')
-    .send({ email: 'thisemailIsInvalid' })
-    .expect(422);
+  beforeAll(() => {
+    password = 'Valid12valid12'
+  });
+
+  it('Should return 422 if the email is not provided',async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({password})
+      .expect(422);
+  });
+
+  it('Should return 422 if the email not valid', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'thisEmailIsNotValid', password })
+      .expect(422);
+  });
+
+  // it('Should return 200 if email is valid',async () => {
+  //   await request(app)
+  //     .post('/api/auth/signup')
+  //     .send({ email: 'test@gmail.com', password })
+  //     .expect(200);
+  // });
 });
+
+
+/**
+ * Valid password condition:
+ * - At least 8 character
+ * - at most 32 character
+ * - One uppercase letter
+ * - One lowercase letter
+ * - One number
+ */ 
+describe('test validity of password input', () => {
+  let email = '';
+
+  beforeAll(()  => {
+    email = 'test@test.com'
+  });
+
+  it('Should return 422 if password is not provided',async () => {
+    await request(app)
+    .post('/api/auth/signup')
+    .send({ email })
+    .expect(422);
+  });
+
+  it('Should return 422 if password contains less than 8 character', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'valid12' })
+      .expect(422);
+  });
+
+  it('Should return 422 if password contains more than 32 character', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'valid12jkasxhkfkhfhfllsfsfshlffhlsahlfasjkgfugkeawfukg' })
+      .expect(422);
+  });
+
+  it('Should return 422 if password does not contains one lowercase letter', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'A2734892470242347' })
+      .expect(422);
+  });
+
+  it('Should return 422 if password does not contains one uppercase letter', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'valid12valid12' })
+      .expect(422);
+  });
+
+  it('Should return 422 if password does not contains number', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email, password: 'Validvalid' })
+      .expect(422);
+  });
+
+  // it('Should return 200 if password valid', async () => {
+  //   await request(app)
+  //     .post('/api/auth/signup')
+  //     .send({ email, password: 'Valid12valid12' })
+  //     .expect(200);
+  // });
+})
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,12 +5,18 @@ const signupRouters = express.Router();
 
 signupRouters.post(
   '/api/auth/signup',
-  [body('email').isEmail().withMessage('Email must be a valid format')],
+  [
+    body('email').isEmail().withMessage('Email must be a valid format'),
+    body('password').trim().isLength({min: 8, max: 32}).withMessage('Password must be between 8 and 32 characters'),
+    body('password').contains(/^(.*[a-z].*)$/).withMessage('Password must contain at least one lower case letter'),
+    body('password').contains(/^(.*[A-Z].*)$/).withMessage('Password must contain at least one upper case letter'),
+    body('password').contains(/^(.*\d.*)$/).withMessage('Password must contain at least one upper case letter')
+  ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      res.status(422).send({});
+      res.status(422).send({errors});
     }
 
     res.send({});
